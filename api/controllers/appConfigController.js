01a4573a4e0c985fd5b55ba03b611c3d52283d0a
Peter Rosenberg <p.rosenberg@gmx.net>
2020-08-13 19:04:54 +0200

    convert to async

--- api/controllers/appConfigController.js
+++ api/controllers/appConfigController.js
@@ -5,6 +5,9 @@ const util = require('util');
 const fs = require('fs');
 const config = require('./config.json');
 
+// Convert fs.readFile into Promise version of the same
+const readFile = util.promisify(fs.readFile);
+
 let generateEntryForPr = function (prNumber, sort) {
     return "- id: PR-" + prNumber + "\n" +
         "  name: " + util.format(config.entryName, prNumber) + "\n" +
@@ -15,41 +18,35 @@ let generateEntryForPr = function (prNumber, sort) {
         "    value: " + util.format(config.entryKeyValueValue, prNumber) + "\n";
 }
 
-exports.fetchConfig = function (req, res) {
+exports.fetchConfig = async function (req, res) {
     const client = github.client(config.githubPersonAccessToken)
     const repo = client.repo(config.githubRepository)
-    repo.prs(function (err, data) {
-        if (err != null) {
-            console.log(err);
-            return
-        }
-
-        let filteredPullRequests = data.filter(function (item) {
-            return item.state === "open";
-        });
-        let pullRequestNumbers = filteredPullRequests.map(function (item) {
-            return item.number;
-        });
-
-        let generatedOutput = "";
-        let sort = 110; // start above 100 to leave space for local entries
-        pullRequestNumbers
-            .sort(function (a, b) {
-                // reverse
-                return b - a;
-            })
-            .forEach(function (item) {
-                generatedOutput += generateEntryForPr(item, sort++);
-            });
-
-        fs.readFile(config.preconfiguredEntriesFile, 'utf8',
-            function (err, data) {
-                if (err) {
-                    // file not found so just return generated output
-                    res.send(generatedOutput);
-                } else {
-                    res.send(data + generatedOutput);
-                }
-            });
+    const prsResult = await repo.prsAsync();
+    const allPullRequests = prsResult[0];
+    let filteredPullRequests = allPullRequests.filter(function (item) {
+        return item.state === "open";
     });
+    let pullRequestNumbers = filteredPullRequests.map(function (item) {
+        return item.number;
+    });
+
+    let generatedOutput = "";
+    let sort = 110; // start above 100 to leave space for local entries
+    pullRequestNumbers
+        .sort(function (a, b) {
+            // reverse
+            return b - a;
+        })
+        .forEach(function (item) {
+            generatedOutput += generateEntryForPr(item, sort++);
+        });
+
+    try {
+        const fileData = await readFile(config.preconfiguredEntriesFile);
+        res.send(fileData + generatedOutput);
+    } catch (e) {
+        console.log("file '" + config.preconfiguredEntriesFile + "' not found. ", e)
+        // file not found so just return generated output
+        res.send(generatedOutput);
+    }
 };
