d750e247dc6b81cfea250c3f765d9ee22e362f94
Peter Rosenberg <p.rosenberg@gmx.net>
2020-08-19 11:53:01 +0200

    add test

--- test/appConfigControllerTest.spec.ts
+++ test/appConfigControllerTest.spec.ts
@@ -15,7 +15,7 @@ const initConfig = function () {
 
 describe('AppConfigService', () => {
 
-    it('without preconfiguration', async () => {
+    it('without pre configuration', async () => {
         // given
         initConfig();
         let result: string = null;
@@ -42,6 +42,62 @@ describe('AppConfigService', () => {
         expect(result).to
             .equal(
                 '- id: PR-32\n'
+                + '  name: PR 32\n'
+                + '  authority: com.test.config\n'
+                + '  sort: 110\n  keyValues:\n'
+                + '  - key: START_URL\n'
+                + '    value: https://pr-32.test.com/\n'
+                + '- id: PR-31\n'
+                + '  name: PR 31\n'
+                + '  authority: com.test.config\n'
+                + '  sort: 111\n  keyValues:\n'
+                + '  - key: START_URL\n'
+                + '    value: https://pr-31.test.com/\n');
+    });
+
+    it('with pre configuration', async () => {
+        // given
+        initConfig();
+        config.preconfiguredEntriesFile = "./test/testPreconfiguredEntries.yaml";
+        let result: string = null;
+        let res = {
+            send(content: string) {
+                result = content;
+            }
+        };
+        client.repo = function () {
+            return {
+                prsAsync() {
+                    return [[
+                        {state: "open", number: 32},
+                        {state: "open", number: 31},
+                        {state: "closed", number: 34}
+                    ]];
+                }
+            }
+        }
+
+        // when
+        await fetchConfigAsync({}, res);
+        // then
+        expect(result).to
+            .equal(
+                '- id: test-prod\n'
+                + '  name: Test Prod\n'
+                + '  authority: com.test.config\n'
+                + '  sort: 100\n'
+                + '  keyValues:\n'
+                + '    - key: START_URL\n'
+                + '      value:\n'
+                + '- id: test-page\n'
+                + '  name: Test Page\n'
+                + '  authority: com.test.config\n'
+                + '  sort: 999\n'
+                + '  keyValues:\n'
+                + '    - key: START_URL\n'
+                + '      value: https://test.com/test-page/\n'
+
+                + '- id: PR-32\n'
                 + '  name: PR 32\n'
                 + '  authority: com.test.config\n'
                 + '  sort: 110\n  keyValues:\n'
